24c48849fb1d52fda662124825f7a986cb2a8eb1
Justin Staubach <justin@staubach.us>
2019-03-04 19:33:51 -0500

    samesite false

--- server.js
+++ server.js
@@ -69,6 +69,7 @@ app.use(
   require('express-session')({
     cookie: {
       domain: process.env.SESSION_DOMAIN || undefined,
+      sameSite: false,
     },
     secret: 'keyboard cat',
     resave: true,
